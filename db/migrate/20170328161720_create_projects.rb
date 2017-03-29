8059e19232f4abca1ba7b661486c0fcb033cb38e
Chandra Shekhar <shekharindian099@gmail.com>
2017-03-29 18:38:24 +0530

    Added project model

---
+++ db/migrate/20170328161720_create_projects.rb
@@ -0,0 +1,16 @@
+class CreateProjects < ActiveRecord::Migration[5.0]
+  def change
+    create_table :projects do |t|
+      t.string :title
+      t.string :overview
+      t.string :image_url
+      t.string :video_url
+      t.string :goal
+      t.string :model
+      t.string :start_date
+      t.string :duration
+
+      t.timestamps
+    end
+  end
+end
