0db0e346737623cf205b3a6f5c9085ebb5d8fd2f
Chandra Shekhar <shekharindian099@gmail.com>
2017-03-30 15:58:34 +0530

    remove binding pry and remove sanitise parmas

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -1,14 +1,11 @@
 module Api
   module  V1
     class ProjectsController < ApplicationController
-      before_action :sanitise_params, only: :create
-      
       def index
       end
 
       def create
         project = Project.new(project_params)
-        binding.pry
         if project.save
           render json: project, status: :created
         else
@@ -28,15 +25,6 @@ module Api
           events_attributes: [:id, :title, :country, :date, :image_url, :description]  
         )
       end
-
-      def sanitise_params
-        params['project']['rewards_attributes'] = params['project']['rewards']
-        params['project']['story_attributes'] = params['project']['story']
-        params['project']['faqs_attributes'] = params['project']['faqs']
-        params['project']['links_attributes'] = params['project']['links']
-        params['project']['events_attributes'] = params['project']['events']
-      end
-
     end
   end
 end
