05e2cbb3d4a65e22cabaa0fa6a0ad2a593251894
Chandra Shekhar <shekharindian099@gmail.com>
2017-04-10 17:57:08 +0530

    add cors for devise token auth

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,9 +1,9 @@
-Rails.application.config.middleware.use Rack::Cors do
-  allow do
-    origins '*'
-    resource '*',
-    :headers => :any,
-    :expose  => ['access-token', 'expiry', 'token-type', 'uid', 'client'],
-    :methods => [:get, :post, :delete, :put, :patch, :options, :head]
-  end
-end
+# Rails.application.config.middleware.use Rack::Cors do
+#   allow do
+#     origins '*'
+#     resource '*',
+#     :headers => :any,
+#     :expose  => ['access-token', 'expiry', 'token-type', 'uid', 'client'],
+#     :methods => [:get, :post, :delete, :put, :patch, :options, :head]
+#   end
+# end
