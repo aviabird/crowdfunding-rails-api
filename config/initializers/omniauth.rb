0a7e61132e47e0de204a87b6503fc255ae72002e
Chandra Shekhar <shekharindian099@gmail.com>
2017-04-12 20:51:32 +0530

    typo bug fixes

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook,      ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET']
-  provider :google_oauth2, ENV['GOOGLE_KEY'],   ENV['GOOGLE_SECRET'], , {
+  provider :google_oauth2, ENV['GOOGLE_KEY'],   ENV['GOOGLE_SECRET'], {
     name: 'google',
     scope: 'email, profile, plus.me',
     prompt: 'select_account',
