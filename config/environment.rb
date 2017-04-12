a8de67caa02153fc8ffdbe07f6e27c3e0bf05d56
Chandra Shekhar <shekharindian099@gmail.com>
2017-04-12 20:54:24 +0530

    bug fix

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,4 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-OmniAuth.config.full_host = Rails.env.production? ? 'https://crowdpouch.herokuapp.com' : 'http://localhost:3000'
+# OmniAuth.config.full_host = Rails.env.production? ? 'https://crowdpouch.herokuapp.com' : 'http://localhost:3000'
