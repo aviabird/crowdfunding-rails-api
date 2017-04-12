4a104ca9d4f55ae86720e3823fa462e16bcae661
Chandra Shekhar <shekharindian099@gmail.com>
2017-04-12 20:49:44 +0530

    add redirect uri

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,16 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook,      ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET']
-  provider :google_oauth2, ENV['GOOGLE_KEY'],   ENV['GOOGLE_SECRET']
+  provider :google_oauth2, ENV['GOOGLE_KEY'],   ENV['GOOGLE_SECRET'], , {
+    name: 'google',
+    scope: 'email, profile, plus.me',
+    prompt: 'select_account',
+    image_aspect_ratio: 'square',
+    image_size: 50,
+    setup: (lambda do |env|
+      request = Rack::Request.new(env)
+      env['omniauth.strategy'].options['token_params'] = {
+        redirect_uri: 'https://crowdpouch.herokuapp.com/auth/google_oauth2/callback'
+      }
+    end)
+  }
 end
