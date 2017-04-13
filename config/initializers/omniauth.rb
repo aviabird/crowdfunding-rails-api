84c3b06ccdecfd75a88abc613badd6ddc3c50886
Chandra Shekhar <shekharindian099@gmail.com>
2017-04-13 11:23:08 +0530

    remove omniauth gem and configurations

--- config/initializers/omniauth.rb
+++
@@ -1,4 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook,      ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET']
-  provider :google_oauth2, ENV['GOOGLE_KEY'],   ENV['GOOGLE_SECRET']
-end
