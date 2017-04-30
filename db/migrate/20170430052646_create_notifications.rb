007d76ec74af22d8ef7d14e452737159c45427c6
Chandra Shekhar <shekharindian099@gmail.com>
2017-04-30 11:18:00 +0530

    add notifications table

---
+++ db/migrate/20170430052646_create_notifications.rb
@@ -0,0 +1,15 @@
+class CreateNotifications < ActiveRecord::Migration[5.0]
+  def change
+    create_table :notifications do |t|
+      t.string :subject
+      t.string :description
+      t.boolean :read_status, default: false
+      t.datetime :read_at
+      t.integer :user_id
+
+      t.timestamps
+    end
+
+    add_index :notifications, :user_id
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170427101634) do
+ActiveRecord::Schema.define(version: 20170430052646) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -88,6 +88,17 @@ ActiveRecord::Schema.define(version: 20170427101634) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["project_id"], name: "index_links_on_project_id", using: :btree
+  end
+
+  create_table "notifications", force: :cascade do |t|
+    t.string   "subject"
+    t.string   "description"
+    t.boolean  "read_status", default: false
+    t.datetime "read_at"
+    t.integer  "user_id"
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.index ["user_id"], name: "index_notifications_on_user_id", using: :btree
   end
 
   create_table "profiles", force: :cascade do |t|
